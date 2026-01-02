dca22620532d037bf87fa0ad6dce720bee3d03af
Diego Muñoz <dmunoz-ext@dinersclub.com.pe>
2026-01-02 14:56:02 -0500

    Crea casos de uso

---
+++ domain/usecase/src/main/java/co/com/nexus/usecase/professionalprofile/GetProfessionalProfileByIdUseCase.java
@@ -0,0 +1,22 @@
+package co.com.nexus.usecase.professionalprofile;
+
+import co.com.nexus.model.professionalprofile.ProfessionalProfileModel;
+import co.com.nexus.model.professionalprofile.gateways.ProfessionalProfileRepository;
+import co.com.nexus.model.shared.constants.HttpStatus;
+import co.com.nexus.model.shared.exception.NexusException;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class GetProfessionalProfileByIdUseCase implements Function<UUID, Mono<ProfessionalProfileModel>> {
+    private final ProfessionalProfileRepository professionalProfileRepository;
+
+    @Override
+    public Mono<ProfessionalProfileModel> apply(UUID uuid) {
+        return professionalProfileRepository.findById(uuid)
+                .switchIfEmpty(Mono.error(new NexusException("PERFIL PROFESIONAL NO ENCONTRADO", HttpStatus.NOT_FOUND)));
+    }
+}
---
+++ domain/usecase/src/main/java/co/com/nexus/usecase/professionalprofile/UpdateProfessionalProfileUseCase.java
@@ -0,0 +1,32 @@
+package co.com.nexus.usecase.professionalprofile;
+
+import co.com.nexus.model.professionalprofile.ProfessionalProfileModel;
+import co.com.nexus.model.professionalprofile.gateways.ProfessionalProfileRepository;
+import co.com.nexus.model.shared.constants.HttpStatus;
+import co.com.nexus.model.shared.exception.NexusException;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class UpdateProfessionalProfileUseCase implements Function<ProfessionalProfileModel, Mono<ProfessionalProfileModel>> {
+    private final ProfessionalProfileRepository professionalProfileRepository;
+
+    @Override
+    public Mono<ProfessionalProfileModel> apply(ProfessionalProfileModel professionalProfileModel) {
+        return professionalProfileRepository.findById(professionalProfileModel.getUserId())
+                .switchIfEmpty(Mono.error(new NexusException("PERFIL PROFESIONAL NO ENCONTRADO", HttpStatus.NOT_FOUND)))
+                .flatMap(existing -> {
+                    existing.setSpecialty(professionalProfileModel.getSpecialty());
+                    existing.setMedicalLicense(professionalProfileModel.getMedicalLicense());
+                    existing.setLicenseCountry(professionalProfileModel.getLicenseCountry());
+                    existing.setLicenseExpirationDate(professionalProfileModel.getLicenseExpirationDate());
+                    existing.setYearsOfExperience(professionalProfileModel.getYearsOfExperience());
+                    existing.setProfessionalBio(professionalProfileModel.getProfessionalBio());
+                    return professionalProfileRepository.save(existing);
+                });
+    }
+}
